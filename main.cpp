9880f081d1ee4c33ebe7646cfb1bbc98a25c7ed2
povilaslai <73555946+povilaslai@users.noreply.github.com>
2021-02-20 20:24:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <random>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::setw;
+
+struct Studentas
+{
+    string vardas;
+    string pavarde;
+    int pazymiai;
+    bool vidurkis;
+    double vidurkis_mediana;
+    int egzaminas;
+    double galutinis_pazymys;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
